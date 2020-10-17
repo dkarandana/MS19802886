2af6de353fa375c797c857dc32b76f3315fbd657
Dhananjaya Karandana <info.idealsolution@gmail.com>
2020-10-17 07:56:29 +0530

    Auth Module Added

---
+++ components/Firebase/firbaseConfig.tsx
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDedn4ZijYs_tBdnlRjXaBKZYqW4TI1pMM",
+    authDomain: "ms19802886-mobile.firebaseapp.com",
+    databaseURL: "https://ms19802886-mobile.firebaseio.com",
+    projectId: "ms19802886-mobile",
+    storageBucket: "ms19802886-mobile.appspot.com",
+    messagingSenderId: "808984137900",
+    appId: "project-808984137900",
+    measurementId: "G-measurement-id"
+};
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,16 +7,16 @@ import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import { AuthUserProvider } from './AuthUserProvider';
+import Routes from './Routes';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
+    <AuthUserProvider>
+      <Routes />
+    </AuthUserProvider>
   );
 }
 
